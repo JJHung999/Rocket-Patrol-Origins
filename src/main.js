5eeed471743aa75d541fb74ffc936e8a525e5d12
JJHung999 <jjhung@ucsc.edu>
2022-04-04 15:58:37 -0700

    adding to the git

---
+++ src/main.js
@@ -0,0 +1,11 @@
+const { Phaser } = require("../lib/phaser");
+
+console.log("Hello from main.js");
+
+let config = {
+    type: Phaser.CANVAS,
+    width: 640, 
+    height: 480
+}
+
+let game = new Phaser.Game();
