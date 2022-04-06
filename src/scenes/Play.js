036d688bdb80009e2336a1096002cdc8c751e333
JJHung999 <jjhung@ucsc.edu>
2022-04-06 16:39:41 -0700

    change on play

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -3,12 +3,23 @@ class Play extends Phaser.scene {
         super("Play");
     }
     preload(){
-        this.preload.image("starfield","assets/starfield.png")
+        this.load.image('rocket', './assets/rocket.png');
+        this.load.image('spaceship', './assets/rocket.png');
+        this.load.image('starfield', './assets/starfield.png');
+
     }
     create(){
-        this.add.tileSprite(20, 20, game.config.width, game.config.heigh, "starfield").setOrigin(0,0)
-        this.add.rectangle(100,100, game.config.width, borderUISize * 2, 0xfacade)
-        this.add.rectangle(100,100, game.config.width, borderUISize * 2, 0xfacade)
+        //background
+        this.add.tileSprite(20, 20, game.config.width, game.config.heigh, "starfield").setOrigin(0,0);
+        
+        //green UI background
+        this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x00FF00).setOrigin(0, 0);
+
+        //white borders
+        this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0 ,0);
+        this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0 ,0);
+        this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0 ,0);
+        this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0 ,0);
     }
 
     update() {
