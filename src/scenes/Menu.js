69d946ae5034349173460561dddf6b2a607c738a
JJHung999 <jjhung@ucsc.edu>
2022-04-07 13:17:11 -0700

    menu revisions

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -4,6 +4,8 @@ class Menu extends Phaser.Scene {
     }
     create(){
         let menutext = this.add.text(300, 240, "menu goes here")
+        menutext.setOrigin(0.5,0.5);
 
+        this.scene.start("Play")
     }
 }
